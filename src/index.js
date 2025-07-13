30a92f8d52d069b5bb1298fcd9b57698deafb45b
Caleb Estevão Kappes Oro <calebekappes2@gmail.com>
2025-07-12 21:56:33 -0300

    Implementa funcionalidade PWA

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'; // Importe o BrowserRouter
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Importe o registrador
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,3 +15,5 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+// Ativa o registro do Service Worker
+serviceWorkerRegistration.register();
---
+++ src/service-worker.js
@@ -0,0 +1,50 @@
+// src/service-worker.js
+
+/* eslint-disable no-restricted-globals */
+
+import { clientsClaim } from 'workbox-core';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
+
+clientsClaim();
+
+// Faz o pré-cache de todos os recursos gerados pelo processo de build.
+// Seus URLs são injetados no manifesto a seguir.
+precacheAndRoute(self.__WB_MANIFEST);
+
+const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
+registerRoute(
+  // Retorna não, se a URL se parece com uma URL para um arquivo na pasta public
+  ({ request, url }) => {
+    if (request.mode !== 'navigate') {
+      return false;
+    }
+    if (url.pathname.startsWith('/_')) {
+      return false;
+    }
+    if (url.pathname.match(fileExtensionRegexp)) {
+      return false;
+    }
+    return true;
+  },
+  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+);
+
+// Exemplo de cache para imagens em tempo de execução
+registerRoute(
+  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
+  new StaleWhileRevalidate({
+    cacheName: 'images',
+    plugins: [
+      new ExpirationPlugin({ maxEntries: 50 }),
+    ],
+  })
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
